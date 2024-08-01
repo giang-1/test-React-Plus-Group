c6ece5fd46050fcbe67b2b2f33449d74258d6e75
giang-1 <giang06121102@gmail.com>
2024-08-01 21:25:50 +0700

    add sweet alert

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import Swal from 'sweetalert2';
 
 function App() {
   const [pointOfChracterA, setPointOfCharacterA] = useState(1)
@@ -16,25 +17,40 @@ function App() {
     setPointOfCharacterA(1)
     setPointOfCharacterB(1)
   }
+  if (pointOfChracterA >= 10) {
+    Swal.fire({
+      title: 'Character A win',
+      icon: "success"
+    });
+    resetCharacterPoint()
+  } else if (pointOfChracterB >= 10) {
+    Swal.fire({
+      title: 'Character B win',
+      icon: "success"
+    });
+    resetCharacterPoint()
+  }
   return (
     <div className="container ">
       <div>
         {
-          pointOfChracterA > pointOfChracterB ? <h4>A is Winner</h4> : pointOfChracterA === pointOfChracterB ? <h4>same point</h4> : <h4>B is Winner</h4>
+          pointOfChracterA > pointOfChracterB ?
+            <h4>A is Winner</h4> : pointOfChracterA === pointOfChracterB ?
+              <h4>Same point</h4> : <h4>B is Winner</h4>
         }
       </div>
       <div>
         <p>character A</p>
         {new Array(pointOfChracterA).fill(1).map((point, index) => (
-          <button className='btn btn-primary' key={index}></button>
+          <button className='btn btn-primary me-1' key={index}></button>
         ))}
         <p>character B</p>
         {new Array(pointOfChracterB).fill(1).map((point, index) => (
-          <button className='btn btn-primary' key={index}></button>
+          <button className='btn btn-primary me-1' key={index}></button>
         ))}
       </div>
       <div>
-        <button className='btn btn-sm btn-success ' onClick={ramdomPlusPointForCharacter}>race</button>
+        <button className='btn btn-sm btn-success me-2' onClick={ramdomPlusPointForCharacter}>race</button>
         {pointOfChracterA >= 2 || pointOfChracterB >= 2 ? <button className='btn btn-sm btn-warning md-2' onClick={resetCharacterPoint}>reset</button> : ''}
       </div>
 
