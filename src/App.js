31e4b7fe3f0734c1cef1692e2c6a836d1b489147
giang-1 <giang06121102@gmail.com>
2024-08-01 20:54:03 +0700

    hoan thanh bai test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pointOfChracterA, setPointOfCharacterA] = useState(1)
+  const [pointOfChracterB, setPointOfCharacterB] = useState(1)
+  const ramdomPlusPointForCharacter = () => {
+    let a = Math.floor(Math.random() * 10)
+    if (a < 5) {
+      setPointOfCharacterA(pointOfChracterA + 1)
+    } else {
+      setPointOfCharacterB(pointOfChracterB + 1)
+    }
+  }
+  const resetCharacterPoint = () => {
+    setPointOfCharacterA(1)
+    setPointOfCharacterB(1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container ">
+      <div>
+        {
+          pointOfChracterA > pointOfChracterB ? <h4>A is Winner</h4> : pointOfChracterA === pointOfChracterB ? <h4>same point</h4> : <h4>B is Winner</h4>
+        }
+      </div>
+      <div>
+        <p>character A</p>
+        {new Array(pointOfChracterA).fill(1).map((point, index) => (
+          <button className='btn btn-primary' key={index}></button>
+        ))}
+        <p>character B</p>
+        {new Array(pointOfChracterB).fill(1).map((point, index) => (
+          <button className='btn btn-primary' key={index}></button>
+        ))}
+      </div>
+      <div>
+        <button className='btn btn-sm btn-success ' onClick={ramdomPlusPointForCharacter}>race</button>
+        {pointOfChracterA >= 2 || pointOfChracterB >= 2 ? <button className='btn btn-sm btn-warning md-2' onClick={resetCharacterPoint}>reset</button> : ''}
+      </div>
+
     </div>
   );
 }
